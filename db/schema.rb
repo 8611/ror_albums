db8a154b30a933fa3ae2c2ce425d143f308cf387
8611pl@gmail.com <8611pl@gmail.com>
2015-04-27 21:25:56 +0200

    Album scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427190658) do
+ActiveRecord::Schema.define(version: 20150427192540) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "artist_id",   limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "date_start"
+    t.datetime "date_end"
+    t.integer  "country_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string   "name",       limit: 255
